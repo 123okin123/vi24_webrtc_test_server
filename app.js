4bf0ff1837675ee4a346cce45c622aa6554f5b4d
Nikolai Kratz <nikolai.kratz@gmail.com>
2017-12-22 22:15:41 +0800

    init2

--- app.js
+++ app.js
@@ -1,46 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+'use strict';
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const SocketServer = require('ws').Server;
+const path = require('path');
 
-var app = express();
+const PORT = process.env.PORT || 3000;
+const INDEX = path.join(__dirname, 'index.html');
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+const server = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+server.use((req, res) => res.sendFile(INDEX));
+server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
-app.use('/', index);
-app.use('/users', users);
+const wss = new SocketServer({ server });
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+wss.on('connection', (ws) => {
+    console.log('Client connected');
+ws.on('close', () => console.log('Client disconnected'));
+ws.on('message', (data) => {
+    console.log(data);
+});
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
 
-module.exports = app;
+// setInterval(() => {
+//   wss.clients.forEach((client) => {
+//     client.send(new Date().toTimeString());
+//   });
+// }, 1000);
