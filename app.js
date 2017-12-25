41cc90b82bc3971b9b649d3e67d04e418f2e5247
Nikolai Kratz <nikolai.kratz@gmail.com>
2017-12-25 18:56:04 +0800

    test

--- app.js
+++ app.js
@@ -7,25 +7,30 @@ const SocketServer = require('ws').Server;
 
 
 const PORT = process.env.PORT || 3000;
-
-
 const server = http.createServer(app);
 
-//server.use((req, res) => res.send('Hello'));
+server.use((req, res) => res.send('Hello'));
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
 
+let clientId = 0;
 wss.on('connection', (ws) => {
-    console.log('Client connected');
+    let id = ++clientId;
+    console.log('Client connected with id: ' + id);
     ws.on('request', (req) => {
         console.log('Client request from origin: ' + req.origin);
     });
     ws.on('close', () => {
-        console.log('Client disconnected');
+        console.log('Client disconnected with id: ' + id);
     });
     ws.on('message', (data) => {
-        console.log(data);
+        console.log('Got message from '+ id +': ' + data);
+        wss.clients.forEach((client) => {
+            if (client !== ws) {
+                client.send(data)
+            }
+        });
 });
 });
 
