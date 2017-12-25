dd5973bd58944593d772d6bc307367159275b714
Nikolai Kratz <nikolai.kratz@gmail.com>
2017-12-25 17:39:22 +0800

    test

--- app.js
+++ app.js
@@ -2,14 +2,14 @@
 
 const express = require('express');
 const SocketServer = require('ws').Server;
-const path = require('path');
+
 
 const PORT = process.env.PORT || 3000;
-const INDEX = path.join(__dirname, 'index.html');
+
 
 const server = express();
 
-server.use((req, res) => res.sendFile(INDEX));
+server.use((req, res) => res.send('Hello'));
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`));
 
 const wss = new SocketServer({ server });
