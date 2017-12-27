901efd3430f81568873ecefedb81c8b85d849b85
Nikolai Kratz <nikolai.kratz@gmail.com>
2017-12-27 18:28:37 +0800

    working

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname+ 'public' + 'index.html'));
 });
-app.get('/keepalive', (req, res) => {
+app.post('/keepalive', (req, res) => {
     res.send('success');
 });
 
